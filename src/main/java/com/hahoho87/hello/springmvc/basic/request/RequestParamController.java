fb49fb91fa7823735503666cb58decbf9052b2a5
bky4564 <bky4564@hunet.co.kr>
2021-07-28 12:38:10 +0900

    http request parameter - 쿼리 파라미터, HTML Form 예제 추가

---
+++ src/main/java/com/hahoho87/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,26 @@
+package com.hahoho87.hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@RestController
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(ServletRequest request, ServletResponse response) throws IOException {
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        log.info("username = {}, age = {}", username, age);
+
+        response.getWriter().write("ok");
+    }
+}
